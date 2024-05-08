918b6a36c4f7ef854b91e45312dce602ca98b532
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-08 19:24:02 +0200

    lfu

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -24,31 +24,38 @@ class LFUCache(BaseCaching):
         if key not in self.cache_data:
             if len(self.cache_data) < BaseCaching.MAX_ITEMS:
                 self.cache_data[key] = item
-                self.items_frequency[key] = 0
+                self.items_frequency[key] = 1  # New item, set frequency to 1
             else:
-                max_frequency = max(self.items_frequency.values())
-                max_freq_keys = [k for k, v in self.items_frequency.items() if v == max_frequency]
-                if len(max_freq_keys) == 1:
-                    least_recently_used_key = max(self.cache_data, key=self.cache_data.get)
-                    print("DISCARD:", least_recently_used_key)
-                    del self.cache_data[least_recently_used_key]
-                    del self.items_frequency[least_recently_used_key]
+                min_frequency = min(self.items_frequency.values())
+                min_freq_keys = [k for k, v in self.items_frequency.items() if v == min_frequency]
+                if len(min_freq_keys) == 1:
+                    least_frequently_used_key = min_freq_keys[0]
+                    print("DISCARD:", least_frequently_used_key)
+                    del self.cache_data[least_frequently_used_key]
+                    del self.items_frequency[least_frequently_used_key]
                 else:
-                    least_recently_used_key = max_freq_keys[0]
-                    for k in max_freq_keys:
-                        if self.items_frequency[k] < self.items_frequency[least_recently_used_key]:
-                            least_recently_used_key = k
-                    print("DISCARD:", least_recently_used_key)
-                    del self.cache_data[least_recently_used_key]
-                    del self.items_frequency[least_recently_used_key]
+                    # If there are multiple keys with the same minimum frequency,
+                    # we choose the least recently used among them (using LRU)
+                    least_frequently_used_key = min_freq_keys[0]
+                    for k in min_freq_keys:
+                        if self.items_frequency[k] < self.items_frequency[least_frequently_used_key]:
+                            least_frequently_used_key = k
+                    print("DISCARD:", least_frequently_used_key)
+                    del self.cache_data[least_frequently_used_key]
+                    del self.items_frequency[least_frequently_used_key]
                 
                 self.cache_data[key] = item
-                self.items_frequency[key] = 0
+                self.items_frequency[key] = 1  # New item, set frequency to 1
+        else:
+            # If key is already in cache, just update the item
+            self.cache_data[key] = item
+            self.items_frequency[key] += 1  # Increment frequency of existing item
+
 
     def get(self, key):
         """ Get an item by key
         """
         if key is None or key not in self.cache_data:
             return None
-        self.items_frequency[key] += 1  # Increment frequency regardless of cache hit or miss
+        self.items_frequency[key] += 1
         return self.cache_data[key]
